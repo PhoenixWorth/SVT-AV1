0933a2a344743ec805b8a2c95685c6d22cac06de
hguermaz <hassen.guermazi@intel.com>
2019-09-25 15:08:37 -0700

    fixed a build warning

--- Source/Lib/Common/Codec/EbRateDistortionCost.h
+++ Source/Lib/Common/Codec/EbRateDistortionCost.h
@@ -220,6 +220,15 @@ extern "C" {
         uint64_t                                 *cr_coeff_bits,
         BlockSize                                bsize);
 
+#if ENHANCE_ATB
+    extern uint64_t get_tx_size_bits(
+        ModeDecisionCandidateBuffer          *candidateBuffer,
+        ModeDecisionContext                  *context_ptr,
+        PictureControlSet                    *picture_control_set_ptr,
+        uint8_t                               tx_depth,
+        EbBool                                block_has_coeff,
+        EbBool                                is_inter);
+#endif
 #ifdef __cplusplus
 }
 #endif
